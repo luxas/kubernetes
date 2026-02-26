5e481d8fabec9365ccd388c8dfbb18d13e5b0dab
Lucas Käldström <lucas.kaldstrom@upbound.io>
2026-02-26 15:15:42 +0200

    register plugin

--- pkg/kubeapiserver/options/plugins.go
+++ pkg/kubeapiserver/options/plugins.go
@@ -22,6 +22,7 @@ package options
 import (
 	"github.com/spf13/pflag"
 
+	"k8s.io/apiserver/pkg/admission/plugin/authorizer/conditionsenforcer"
 	mutatingadmissionpolicy "k8s.io/apiserver/pkg/admission/plugin/policy/mutating"
 	validatingadmissionpolicy "k8s.io/apiserver/pkg/admission/plugin/policy/validating"
 
@@ -105,6 +106,7 @@ var AllOrderedPlugins = []string{
 
 	mutatingadmissionpolicy.PluginName,   // MutatingAdmissionPolicy
 	mutatingwebhook.PluginName,           // MutatingAdmissionWebhook
+	conditionsenforcer.PluginName,        // AuthorizationConditionsEnforcer
 	validatingadmissionpolicy.PluginName, // ValidatingAdmissionPolicy
 	validatingwebhook.PluginName,         // ValidatingAdmissionWebhook
 	resourcequota.PluginName,             // ResourceQuota
