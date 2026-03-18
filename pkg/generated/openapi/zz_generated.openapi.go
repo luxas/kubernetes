3d7ba674865c649a20d2f9443aff19edf3d5cb4b
Lucas Käldström <lucas.kaldstrom@upbound.io>
2026-03-18 09:47:27 +0200

    autogenerated

--- pkg/generated/openapi/zz_generated.openapi.go
+++ pkg/generated/openapi/zz_generated.openapi.go
@@ -14141,13 +14141,13 @@ func schema_k8sio_api_authorization_v1alpha1_AuthorizationConditionsReview(ref c
 					},
 					"request": {
 						SchemaProps: spec.SchemaProps{
-							Description: "Request describes the attributes for the authorization conditions request.",
+							Description: "request describes the attributes for the authorization conditions request.",
 							Ref:         ref(authorizationv1alpha1.AuthorizationConditionsRequest{}.OpenAPIModelName()),
 						},
 					},
 					"response": {
 						SchemaProps: spec.SchemaProps{
-							Description: "Response describes the attributes for the authorization conditions response.",
+							Description: "response describes the attributes for the authorization conditions response.",
 							Ref:         ref(authorizationv1alpha1.AuthorizationConditionsResponse{}.OpenAPIModelName()),
 						},
 					},
@@ -14168,40 +14168,40 @@ func schema_k8sio_api_authorization_v1alpha1_AuthorizationConditionsTargetAdmiss
 				Properties: map[string]spec.Schema{
 					"requestKind": {
 						SchemaProps: spec.SchemaProps{
-							Description: "RequestKind is the fully-qualified type of the original API request (for example, v1.Pod or autoscaling.v1.Scale). If this is specified and differs from the value in \"kind\", an equivalent match and conversion was performed.\n\nFor example, if deployments can be modified via apps/v1 and apps/v1beta1, and a webhook registered a rule of `apiGroups:[\"apps\"], apiVersions:[\"v1\"], resources: [\"deployments\"]` and `matchPolicy: Equivalent`, an API request to apps/v1beta1 deployments would be converted and sent to the webhook with `kind: {group:\"apps\", version:\"v1\", kind:\"Deployment\"}` (matching the rule the webhook registered for), and `requestKind: {group:\"apps\", version:\"v1beta1\", kind:\"Deployment\"}` (indicating the kind of the original API request).\n\nSee documentation for the \"matchPolicy\" field in the webhook configuration type for more details.",
+							Description: "requestKind is the fully-qualified type of the original API request (for example, v1.Pod or autoscaling.v1.Scale). If this is specified and differs from the value in \"kind\", an equivalent match and conversion was performed.\n\nFor example, if deployments can be modified via apps/v1 and apps/v1beta1, and a webhook registered a rule of `apiGroups:[\"apps\"], apiVersions:[\"v1\"], resources: [\"deployments\"]` and `matchPolicy: Equivalent`, an API request to apps/v1beta1 deployments would be converted and sent to the webhook with `kind: {group:\"apps\", version:\"v1\", kind:\"Deployment\"}` (matching the rule the webhook registered for), and `requestKind: {group:\"apps\", version:\"v1beta1\", kind:\"Deployment\"}` (indicating the kind of the original API request).\n\nSee documentation for the \"matchPolicy\" field in the webhook configuration type for more details.",
 							Ref:         ref(metav1.GroupVersionKind{}.OpenAPIModelName()),
 						},
 					},
 					"requestResource": {
 						SchemaProps: spec.SchemaProps{
-							Description: "RequestResource is the fully-qualified resource of the original API request (for example, v1.pods). If this is specified and differs from the value in \"resource\", an equivalent match and conversion was performed.\n\nFor example, if deployments can be modified via apps/v1 and apps/v1beta1, and a webhook registered a rule of `apiGroups:[\"apps\"], apiVersions:[\"v1\"], resources: [\"deployments\"]` and `matchPolicy: Equivalent`, an API request to apps/v1beta1 deployments would be converted and sent to the webhook with `resource: {group:\"apps\", version:\"v1\", resource:\"deployments\"}` (matching the resource the webhook registered for), and `requestResource: {group:\"apps\", version:\"v1beta1\", resource:\"deployments\"}` (indicating the resource of the original API request).\n\nSee documentation for the \"matchPolicy\" field in the webhook configuration type.",
+							Description: "requestResource is the fully-qualified resource of the original API request (for example, v1.pods). If this is specified and differs from the value in \"resource\", an equivalent match and conversion was performed.\n\nFor example, if deployments can be modified via apps/v1 and apps/v1beta1, and a webhook registered a rule of `apiGroups:[\"apps\"], apiVersions:[\"v1\"], resources: [\"deployments\"]` and `matchPolicy: Equivalent`, an API request to apps/v1beta1 deployments would be converted and sent to the webhook with `resource: {group:\"apps\", version:\"v1\", resource:\"deployments\"}` (matching the resource the webhook registered for), and `requestResource: {group:\"apps\", version:\"v1beta1\", resource:\"deployments\"}` (indicating the resource of the original API request).\n\nSee documentation for the \"matchPolicy\" field in the webhook configuration type.",
 							Ref:         ref(metav1.GroupVersionResource{}.OpenAPIModelName()),
 						},
 					},
 					"requestSubResource": {
 						SchemaProps: spec.SchemaProps{
-							Description: "RequestSubResource is the name of the subresource of the original API request, if any (for example, \"status\" or \"scale\") If this is specified and differs from the value in \"subResource\", an equivalent match and conversion was performed. See documentation for the \"matchPolicy\" field in the webhook configuration type.",
+							Description: "requestSubResource is the name of the subresource of the original API request, if any (for example, \"status\" or \"scale\") If this is specified and differs from the value in \"subResource\", an equivalent match and conversion was performed. See documentation for the \"matchPolicy\" field in the webhook configuration type.",
 							Type:        []string{"string"},
 							Format:      "",
 						},
 					},
 					"name": {
 						SchemaProps: spec.SchemaProps{
-							Description: "Name is the name of the object as presented in the request. On a CREATE operation, the client may omit name and rely on the server to generate the name. If that is the case, this field will contain an empty string.",
+							Description: "name is the name of the object as presented in the request. On a CREATE operation, the client may omit name and rely on the server to generate the name. If that is the case, this field will contain an empty string.",
 							Type:        []string{"string"},
 							Format:      "",
 						},
 					},
 					"namespace": {
 						SchemaProps: spec.SchemaProps{
-							Description: "Namespace is the namespace associated with the request (if any).",
+							Description: "namespace is the namespace associated with the request (if any).",
 							Type:        []string{"string"},
 							Format:      "",
 						},
 					},
 					"operation": {
 						SchemaProps: spec.SchemaProps{
-							Description: "Operation is the operation being performed. This may be different than the operation requested. e.g. a patch can result in either a CREATE or UPDATE Operation.",
+							Description: "operation is the operation being performed. This may be different than the operation requested. e.g. a patch can result in either a CREATE or UPDATE Operation.",
 							Default:     "",
 							Type:        []string{"string"},
 							Format:      "",
@@ -14209,33 +14209,33 @@ func schema_k8sio_api_authorization_v1alpha1_AuthorizationConditionsTargetAdmiss
 					},
 					"userInfo": {
 						SchemaProps: spec.SchemaProps{
-							Description: "UserInfo is information about the requesting user",
+							Description: "userInfo is information about the requesting user",
 							Default:     map[string]interface{}{},
 							Ref:         ref(authenticationv1.UserInfo{}.OpenAPIModelName()),
 						},
 					},
 					"object": {
 						SchemaProps: spec.SchemaProps{
-							Description: "Object is the object from the incoming request.",
+							Description: "object is the object from the incoming request.",
 							Ref:         ref(runtime.RawExtension{}.OpenAPIModelName()),
 						},
 					},
 					"oldObject": {
 						SchemaProps: spec.SchemaProps{
-							Description: "OldObject is the existing object. Only populated for DELETE and UPDATE requests.",
+							Description: "oldObject is the existing object. Only populated for DELETE and UPDATE requests.",
 							Ref:         ref(runtime.RawExtension{}.OpenAPIModelName()),
 						},
 					},
 					"dryRun": {
 						SchemaProps: spec.SchemaProps{
-							Description: "DryRun indicates that modifications will definitely not be persisted for this request. Defaults to false.",
+							Description: "dryRun indicates that modifications will definitely not be persisted for this request. Defaults to false.",
 							Type:        []string{"boolean"},
 							Format:      "",
 						},
 					},
 					"options": {
 						SchemaProps: spec.SchemaProps{
-							Description: "Options is the operation option structure of the operation being performed. e.g. `meta.k8s.io/v1.DeleteOptions` or `meta.k8s.io/v1.CreateOptions`. This may be different than the options the caller provided. e.g. for a patch request the performed Operation might be a CREATE, in which case the Options will a `meta.k8s.io/v1.CreateOptions` even though the caller provided `meta.k8s.io/v1.PatchOptions`.",
+							Description: "options is the operation option structure of the operation being performed. e.g. `meta.k8s.io/v1.DeleteOptions` or `meta.k8s.io/v1.CreateOptions`. This may be different than the options the caller provided. e.g. for a patch request the performed Operation might be a CREATE, in which case the Options will a `meta.k8s.io/v1.CreateOptions` even though the caller provided `meta.k8s.io/v1.PatchOptions`.",
 							Ref:         ref(runtime.RawExtension{}.OpenAPIModelName()),
 						},
 					},
