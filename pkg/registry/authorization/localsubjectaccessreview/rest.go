c067672fe22e19ca342ecb58b279726aea9a3864
Lucas Käldström <lucas.kaldstrom@upbound.io>
2026-07-19 20:10:34 +0300

    Make the SubjectAccessReview handlers conditions-aware

--- pkg/registry/authorization/localsubjectaccessreview/rest.go
+++ pkg/registry/authorization/localsubjectaccessreview/rest.go
@@ -24,20 +24,23 @@ import (
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/util/sets"
 	authorizationvalidation "k8s.io/apiserver/pkg/apis/authorization/validation"
 	"k8s.io/apiserver/pkg/authorization/authorizer"
 	genericapirequest "k8s.io/apiserver/pkg/endpoints/request"
+	genericfeatures "k8s.io/apiserver/pkg/features"
 	"k8s.io/apiserver/pkg/registry/rest"
+	utilfeature "k8s.io/apiserver/pkg/util/feature"
 	authorizationapi "k8s.io/kubernetes/pkg/apis/authorization"
 	authorizationutil "k8s.io/kubernetes/pkg/registry/authorization/util"
 )
 
 type REST struct {
-	authorizer authorizer.UnconditionalAuthorizer
+	authorizer authorizer.Authorizer
 	scheme     *runtime.Scheme
 }
 
-func NewREST(authorizer authorizer.UnconditionalAuthorizer, scheme *runtime.Scheme) *REST {
+func NewREST(authorizer authorizer.Authorizer, scheme *runtime.Scheme) *REST {
 	return &REST{authorizer, scheme}
 }
 
@@ -64,16 +67,26 @@ func (r *REST) GetSingularName() string {
 func (r *REST) Create(ctx context.Context, obj runtime.Object, createValidation rest.ValidateObjectFunc, options *metav1.CreateOptions) (runtime.Object, error) {
 	localSubjectAccessReview, ok := obj.(*authorizationapi.LocalSubjectAccessReview)
 	if !ok {
-		return nil, apierrors.NewBadRequest(fmt.Sprintf("not a LocaLocalSubjectAccessReview: %#v", obj))
+		return nil, apierrors.NewBadRequest(fmt.Sprintf("not a LocalSubjectAccessReview: %#v", obj))
 	}
 
-	// The hand-written validations are written only once, for the most recent external API version, so that also k8s.io/apiserver
-	// importers can make use of the validations.
+	// Clear status so it's not taken into account during input validation.
+	// This is important, as we cannot make validation stricter; in k8s 1.36 and before, the client was able to pass a bogus status without a validation error.
+	localSubjectAccessReview.Status = authorizationapi.SubjectAccessReviewStatus{}
+
+	// Clear the options for opting into conditions-awareness when the feature gate is off.
+	// This means that we fallback to the conditions-unaware Authorize when the feature gate is
+	// off, even though both the client and authorizer might have supported conditions.
+	if !utilfeature.DefaultFeatureGate.Enabled(genericfeatures.ConditionalAuthorization) {
+		localSubjectAccessReview.Spec.AuthorizationOptions = nil
+	}
+
 	localSubjectAccessReviewV1 := &authorizationv1.LocalSubjectAccessReview{}
 	if err := r.scheme.Convert(localSubjectAccessReview, localSubjectAccessReviewV1, nil); err != nil {
 		return nil, fmt.Errorf("unexpected, could not convert internal LocalSubjectAccessReview to v1: %w", err)
 	}
 
+	// TODO(luxas): Add an APIVersion parameter here, to run the declarative validation against the right version {v1, v1beta1, v1alpha1}
 	if errs := authorizationvalidation.ValidateLocalSubjectAccessReviewCreate(ctx, r.scheme, localSubjectAccessReviewV1); len(errs) > 0 {
 		return nil, apierrors.NewInvalid(authorizationapi.Kind(localSubjectAccessReview.Kind), "", errs)
 	}
@@ -92,14 +105,26 @@ func (r *REST) Create(ctx context.Context, obj runtime.Object, createValidation 
 	}
 
 	authorizationAttributes := authorizationutil.AuthorizationAttributesFrom(localSubjectAccessReview.Spec)
-	decision, reason, evaluationErr := r.authorizer.Authorize(ctx, authorizationAttributes)
 
-	localSubjectAccessReview.Status = authorizationapi.SubjectAccessReviewStatus{
-		Allowed: (decision == authorizer.DecisionAllow),
-		Denied:  (decision == authorizer.DecisionDeny),
-		Reason:  reason,
+	// Only activate Conditional Authorization if both the server and client supports it
+	if utilfeature.DefaultFeatureGate.Enabled(genericfeatures.ConditionalAuthorization) && localSubjectAccessReviewV1.Spec.AuthorizationOptions.SupportsConditionalAuthorization() {
+		conditionsAwareDecision := r.authorizer.ConditionsAwareAuthorize(ctx, authorizationAttributes)
+		localSubjectAccessReview.Status = authorizationutil.ConditionsAwareDecisionToSARStatus(ctx, authorizationAttributes, conditionsAwareDecision)
+
+	} else if localSubjectAccessReviewV1.Spec.AuthorizationOptions.SupportsUnconditionalAuthorization() {
+		// conditions-unaware flow, feature gate is off or client does not support conditions
+		decision, reason, evaluationErr := r.authorizer.Authorize(ctx, authorizationAttributes)
+
+		localSubjectAccessReview.Status = authorizationapi.SubjectAccessReviewStatus{
+			Allowed:         (decision == authorizer.DecisionAllow),
+			Denied:          (decision == authorizer.DecisionDeny),
+			Reason:          reason,
+			EvaluationError: authorizationutil.BuildEvaluationError(evaluationErr, authorizationAttributes),
+		}
+	} else {
+		// the HandledDecisionTypes was neither [Allow, ConditionsMap, Deny, NoOpinion, Union] or [Allow, Deny, NoOpinion], reject it.
+		return nil, apierrors.NewBadRequest(fmt.Sprintf("unsupported client-handled decision types: %v", sets.List(localSubjectAccessReviewV1.Spec.AuthorizationOptions.GetHandledDecisionTypes())))
 	}
-	localSubjectAccessReview.Status.EvaluationError = authorizationutil.BuildEvaluationError(evaluationErr, authorizationAttributes)
 
 	return localSubjectAccessReview, nil
 }
--- pkg/registry/authorization/subjectaccessreview/rest.go
+++ pkg/registry/authorization/subjectaccessreview/rest.go
@@ -24,19 +24,22 @@ import (
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/util/sets"
 	authorizationvalidation "k8s.io/apiserver/pkg/apis/authorization/validation"
 	"k8s.io/apiserver/pkg/authorization/authorizer"
+	genericfeatures "k8s.io/apiserver/pkg/features"
 	"k8s.io/apiserver/pkg/registry/rest"
+	utilfeature "k8s.io/apiserver/pkg/util/feature"
 	authorizationapi "k8s.io/kubernetes/pkg/apis/authorization"
 	authorizationutil "k8s.io/kubernetes/pkg/registry/authorization/util"
 )
 
 type REST struct {
-	authorizer authorizer.UnconditionalAuthorizer
+	authorizer authorizer.Authorizer
 	scheme     *runtime.Scheme
 }
 
-func NewREST(authorizer authorizer.UnconditionalAuthorizer, scheme *runtime.Scheme) *REST {
+func NewREST(authorizer authorizer.Authorizer, scheme *runtime.Scheme) *REST {
 	return &REST{authorizer, scheme}
 }
 
@@ -66,8 +69,17 @@ func (r *REST) Create(ctx context.Context, obj runtime.Object, createValidation 
 		return nil, apierrors.NewBadRequest(fmt.Sprintf("not a SubjectAccessReview: %#v", obj))
 	}
 
-	// The hand-written validations are written only once, for the most recent external API version, so that also k8s.io/apiserver
-	// importers can make use of the validations.
+	// Clear status so it's not taken into account during input validation.
+	// This is important, as we cannot make validation stricter; in k8s 1.36 and before, the client was able to pass a bogus status without a validation error.
+	subjectAccessReview.Status = authorizationapi.SubjectAccessReviewStatus{}
+
+	// Clear the options for opting into conditions-awareness when the feature gate is off.
+	// This means that we fallback to the conditions-unaware Authorize when the feature gate is
+	// off, even though both the client and authorizer might have supported conditions.
+	if !utilfeature.DefaultFeatureGate.Enabled(genericfeatures.ConditionalAuthorization) {
+		subjectAccessReview.Spec.AuthorizationOptions = nil
+	}
+
 	subjectAccessReviewV1 := &authorizationv1.SubjectAccessReview{}
 	if err := r.scheme.Convert(subjectAccessReview, subjectAccessReviewV1, nil); err != nil {
 		return nil, fmt.Errorf("unexpected, could not convert internal SubjectAccessReview to v1: %w", err)
@@ -84,14 +96,26 @@ func (r *REST) Create(ctx context.Context, obj runtime.Object, createValidation 
 	}
 
 	authorizationAttributes := authorizationutil.AuthorizationAttributesFrom(subjectAccessReview.Spec)
-	decision, reason, evaluationErr := r.authorizer.Authorize(ctx, authorizationAttributes)
 
-	subjectAccessReview.Status = authorizationapi.SubjectAccessReviewStatus{
-		Allowed: (decision == authorizer.DecisionAllow),
-		Denied:  (decision == authorizer.DecisionDeny),
-		Reason:  reason,
+	// Only activate Conditional Authorization if both the server and client supports it
+	if utilfeature.DefaultFeatureGate.Enabled(genericfeatures.ConditionalAuthorization) && subjectAccessReviewV1.Spec.AuthorizationOptions.SupportsConditionalAuthorization() {
+		conditionsAwareDecision := r.authorizer.ConditionsAwareAuthorize(ctx, authorizationAttributes)
+		subjectAccessReview.Status = authorizationutil.ConditionsAwareDecisionToSARStatus(ctx, authorizationAttributes, conditionsAwareDecision)
+
+	} else if subjectAccessReviewV1.Spec.AuthorizationOptions.SupportsUnconditionalAuthorization() {
+		// conditions-unaware flow, feature gate is off or client does not support conditions
+		decision, reason, evaluationErr := r.authorizer.Authorize(ctx, authorizationAttributes)
+
+		subjectAccessReview.Status = authorizationapi.SubjectAccessReviewStatus{
+			Allowed:         (decision == authorizer.DecisionAllow),
+			Denied:          (decision == authorizer.DecisionDeny),
+			Reason:          reason,
+			EvaluationError: authorizationutil.BuildEvaluationError(evaluationErr, authorizationAttributes),
+		}
+	} else {
+		// the HandledDecisionTypes was neither [Allow, ConditionsMap, Deny, NoOpinion, Union] or [Allow, Deny, NoOpinion], reject it.
+		return nil, apierrors.NewBadRequest(fmt.Sprintf("unsupported client-handled decision types: %v", sets.List(subjectAccessReviewV1.Spec.AuthorizationOptions.GetHandledDecisionTypes())))
 	}
-	subjectAccessReview.Status.EvaluationError = authorizationutil.BuildEvaluationError(evaluationErr, authorizationAttributes)
 
 	return subjectAccessReview, nil
 }
