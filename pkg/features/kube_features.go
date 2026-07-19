bcf2115cf9441c37ca2f122a3df4996177d93cdc
Lucas Käldström <lucas.kaldstrom@upbound.io>
2026-07-19 13:30:39 +0300

    Add ConditionalAuthorization feature gate

--- pkg/features/kube_features.go
+++ pkg/features/kube_features.go
@@ -2189,6 +2189,10 @@ var defaultVersionedKubernetesFeatureGates = map[featuregate.Feature]featuregate
 		{Version: version.MustParse("1.37"), Default: true, PreRelease: featuregate.Beta},
 	},
 
+	genericfeatures.ConditionalAuthorization: {
+		{Version: version.MustParse("1.37"), Default: false, PreRelease: featuregate.Alpha},
+	},
+
 	genericfeatures.ConsistentListFromCacheSkipTimeoutFallback: {
 		{Version: version.MustParse("1.37"), Default: false, PreRelease: featuregate.Alpha},
 	},
@@ -2734,6 +2738,8 @@ var defaultKubernetesFeatureGateDependencies = map[featuregate.Feature][]feature
 	genericfeatures.CBORServingAndStorage: {},
 
 	genericfeatures.ConcurrentWatchObjectDecode: {},
+
+	genericfeatures.ConditionalAuthorization: {},
 
 	genericfeatures.ConsistentListFromCacheSkipTimeoutFallback: {},
 
